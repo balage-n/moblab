33b7147ebe619f0be66a4259493fd4910bd6988c
bali93 <balazs.nagy1993@gmail.com>
2017-03-24 09:50:37 +0100

    created mvp base classes

---
+++ app/src/main/java/hu/bme/aut/moblab/ui/Presenter.java
@@ -0,0 +1,18 @@
+package hu.bme.aut.moblab.ui;
+
+/**
+ * Created by bali on 2017. 03. 24..
+ */
+
+public abstract class Presenter<S> {
+    protected S screen;
+
+    public void attachScreen(S screen) {
+        this.screen = screen;
+    }
+
+    public void detachScreen() {
+        this.screen = null;
+    }
+}
+
---
+++ app/src/main/java/hu/bme/aut/moblab/ui/main/MainScreen.java
@@ -0,0 +1,9 @@
+package hu.bme.aut.moblab.ui.main;
+
+/**
+ * Created by bali on 2017. 03. 24..
+ */
+
+public interface MainScreen {
+    void showMessage(String text);
+}
